402dd3932d3f2d10004546c3c7a1828f49a06676
Pawan-Ratwana <ratwanapawan@gmail.com>
2023-08-20 13:06:14 +0530

    some update

--- controller/admin.js
+++ controller/admin.js
@@ -11,24 +11,43 @@ module.exports.addEmployeePage = async function (req, res) {
 }
 module.exports.addEmployee = async function (req, res) {
     try {
-        const employeePresent = await Employee.find({ email: req.body.email });
-        if (employeePresent.length > 0 && employeePresent) {
-            req.flash('error', 'Already Added !!');
-            return res.redirect('/admin_employee/employee_dashboard');
-        } else {
-            const addemployee = new Employee(req.body);
-            await addemployee.save();
-            // now add the employee to all admin
-            var allAdmin = await Admin.findOne({ email: req.cookies.id });
-            allAdmin.employeeId.push(addemployee.id);
-            await allAdmin.save();
-            req.flash('success', 'Employee added successfully !!');
+        const employeeByEmail = await Employee.findOne({ email: req.body.email });
+
+        if (employeeByEmail) {
+            req.flash('error', 'Employee with this email already added');
             return res.redirect('/admin_employee/employee_dashboard');
         }
+
+        const employeeByMobile = await Employee.findOne({ mobile: req.body.mobile });
+
+        if (employeeByMobile) {
+            req.flash('error', 'Employee with this mobile number already added');
+            return res.redirect('/admin_employee/employee_dashboard');
+        }
+
+        const addemployee = new Employee(req.body);
+        
+        // Validate and handle the mobile field
+        if (!addemployee.mobile) {
+            addemployee.mobile = null; // Set to null if mobile is empty
+        }
+
+        await addemployee.save();
+
+        // Add the employee to the admin's list
+        const allAdmin = await Admin.findOne({ email: req.cookies.id });
+        allAdmin.employeeId.push(addemployee.id);
+        await allAdmin.save();
+
+        req.flash('success', 'Employee added successfully');
+        return res.redirect('/admin_employee/employee_dashboard');
     } catch (error) {
-        return res.send("Error in adding employee by admin");
+        console.error("Error in adding employee by admin:", error);
+        req.flash('error', 'Error in adding employee');
+        return res.redirect('/admin_employee/employee_dashboard');
     }
 }
+
 
 module.exports.deleteEmployee = async function (req, res) {
     try {
@@ -58,21 +77,38 @@ module.exports.deleteEmployee = async function (req, res) {
 module.exports.updateEmployee = async function (req, res) {
     try {
         const employeePresent = await Employee.findOne({ email: req.body.email });
+
         if (!employeePresent) {
             req.flash('error', 'Employee Email Not Found');
             return res.redirect('/admin_employee/employee_dashboard');
-        }else{
+        } else {
+            // Update name and password
             employeePresent.name = req.body.name;
             employeePresent.password = req.body.password;
-            employeePresent.save();
-            req.flash('success', 'Updated Success fully !!');
+
+            // Update mobile number if provided, otherwise keep the same
+            if (req.body.mobile) {
+                employeePresent.mobile = req.body.mobile;
+            }
+
+            // Update address if provided, otherwise keep the same
+            if (req.body.city) {
+                employeePresent.city = req.body.city;
+            }
+
+            await employeePresent.save();
+
+            req.flash('success', 'Updated Successfully!');
             return res.redirect('/admin_employee/employee_dashboard');
         }
 
     } catch (error) {
-        return res.send("Error in updating Employee !!");
+        console.error("Error in updating Employee:", error);
+        req.flash('error', 'Error in updating Employee');
+        return res.redirect('/admin_employee/employee_dashboard');
     }
-}
+};
+
 
 //admin performance page
 module.exports.adminPerformancePage = async function (req, res) {
