f2ba9352a372f6cff266650becf549d7429b539e
Pawan-Ratwana <ratwanapawan@gmail.com>
2023-08-21 19:25:20 +0530

    improve

--- controller/employee.js
+++ controller/employee.js
@@ -32,48 +32,71 @@ module.exports.register = async function (req, res) {
         return res.send("<h1>Error in SignUp !!</h1>");
     }
 }
+
+
+
+
 module.exports.performanceReviewList = async function (req, res) {
     try {
-        var allEmployee = await Employee.findOne({ email: req.cookies.id });
-        var feedback = allEmployee.feedback;
-        var list = [];
-        for (var i = 0; i < feedback.length; i++) {
-            const data = await Employee.findById(feedback[i]);
-            if (data != null) {
+        const allEmployee = await Employee.findOne({ email: req.cookies.id });
+
+        if (!allEmployee) {
+            return res.status(404).send("Employee not found");
+        }
+
+        const feedback = allEmployee.feedback;
+        const list = [];
+
+        const employeePromises = feedback.map(async feedbackId => {
+            const data = await Employee.findById(feedbackId);
+            if (data) {
                 list.push(data);
             }
-        }
+        });
+
+        await Promise.all(employeePromises);
+
         res.render('employeeDashboard', {
             title: "Employee",
             allEmployee: list
         });
     } catch (error) {
-        return res.send("<h1>Error in sendiding data to Employee Dashboard !!</h1>");
+        console.error("Error in performanceReviewList:", error);
+        return res.status(500).send("<h1>Error in sending data to Employee Dashboard!</h1>");
     }
 }
 
 
 
+
+
 module.exports.submitFeedback = async function (req, res) {
     try {
+        // Create and save a new Performance document
         var addPerformance = await Performance(req.body);
         addPerformance.save();
-        // find employee by id 
+
+        // Find employee by ID and update their status
         const employeeById = await Employee.findById(req.body.employees);
         employeeById.like = true;
         employeeById.performances = addPerformance.id;
         await employeeById.save();
+
+        // Find all admin documents and update their performance array
         const allAdmin = await Admin.find({});
         for (var i = 0; i < allAdmin.length; i++) {
             allAdmin[i].performance.push(addPerformance.id);
             allAdmin[i].save();
         }
+
+        // Flash success message and redirect
         req.flash('success', 'Feedback Submitted Successfully !!');
         return res.redirect('/employee/perfromancelist');
     } catch (error) {
         return res.send("<h1>Error on Submitting Feedback !!</h1>");
     }
 }
+
 module.exports.feedbackPage = async function (req, res) {
     try {
         return res.render('submitFeedback', {
